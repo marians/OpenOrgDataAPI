766221dbcb05a43f2dabb3b57bfed69786d9a35c
Marian Steinbach <marian@sendung.de>
2013-02-23 15:21:07 +0100

    more

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 
-$es_url = 'http://127.0.0.1:9200/';
+$es_url = 'http://127.0.0.1:9200/orgdata/organisations/_search';
 
 echo "Hello World";
+
+function do_request($url, $method, $data) {
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	return $response;
+}
+
+echo do_request($es_url);
 
 ?>
