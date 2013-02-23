37485337d1eca3530723ec58671ff605113d162d
Marian Steinbach <marian@sendung.de>
2013-02-23 14:59:39 +0100

    debugging

--- dispatch.py
+++ dispatch.py
@@ -8,9 +8,17 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
+print "default"
+
+@app.route('/')
+def home():
+    print "home"
+    return 'Hallo Home!'
+
 
 @app.route('/<foo>')
 def hello_world(foo):
+    print "hello_world"
     return 'Hallo %s' % foo
 
 if __name__ == '__main__':
