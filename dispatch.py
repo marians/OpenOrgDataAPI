e440237cca06795a7129e139567041b2c21c9b90
Marian Steinbach <marian@sendung.de>
2013-02-23 13:13:45 +0100

    WSGI!

--- dispatch.py
+++ dispatch.py
@@ -2,6 +2,7 @@
 
 # encoding: utf-8
 
+from flup.server.fcgi import WSGIServer
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,4 +12,4 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    WSGIServer(app).run()
