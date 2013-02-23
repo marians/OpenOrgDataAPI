cc1c75d59fc5e324b690cc7c1598698e40b4df40
Marian Steinbach <marian@sendung.de>
2013-02-23 15:44:00 +0100

    more

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 
 error_reporting(0);
 
-$es_url = 'http://127.0.0.1:9200/orgdata/organisations/_search';
+$es_url = 'http://127.0.0.1:9200/orgdata/organisation/_search';
 
-function do_request($url, $method, $data) {
+function do_request($url, $method, $query) {
+	$params = array(
+		'query' => array("query_string" => array(
+			"query" => $query
+		)),
+		'facets' => array(
+			'states' => array(
+				'terms' => array('field' => 'state')
+			)
+		)
+	);
+	$params_json = json_encode($params);
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $params_json);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$response = curl_exec($ch);
 	curl_close($ch);
