d2a9b52d91dfc5456f6acab0c9086fe01f3948c9
Marian Steinbach <marian@sendung.de>
2013-02-24 09:21:36 +0100

    Documentation, comment cleanup

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
 
+
+/***************************************************************
+
+  This is an interface/proxy to expose specific ElasticSearch
+  search results as an API with JSON/JSONP output.
+
+  The API supports the following URL parameters in GET
+  requests:
+
+  q - An optional search term. Returned figures deal only with
+      entries matching this quer term.
+
+  callback - This optional paramter can contain the name of a
+      callback function. If this parameter is set, the output
+      is returned as JSONP. Otherwise, JSON is returned.
+  
+  TODO:
+  - Handle bad input and return corresponding HTTP status
+  - Clean up response format
+  - Return relative (density) figures in addition to absolute
+    hit numbers. E.g. in addition to total hits in a state,
+    also (a) the number of hits divided by the number of all entries
+    for that state (float) should be returned. And (b) the number 
+    of entries per capita (float).
+
+  Author: Marian Steinbach
+
+****************************************************************/
+
 error_reporting(0);
 
 $es_url = 'http://127.0.0.1:9200/orgdata/organisation/_search?size=0';
@@ -27,13 +56,7 @@ function do_request($url, $method, $query) {
 			)
 		)
 	);
-	//$params = array(
-	//	'query' => array('query_string' => array(
-	//		'query' => $query
-	//	))
-	//);
 	$params_json = json_encode($params);
-	//print $params_json;
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $params_json);
@@ -56,6 +79,6 @@ if (isset($_GET['callback']) && !empty($_GET['callback'])) {
 	$jsonp_before = $_GET['callback'] . '(';
 	$jsonp_after = ')';
 }
-echo $jsonp_before . do_request($es_url, 'POST', $q) . $jsonp_after ;
+echo $jsonp_before . do_request($es_url, 'POST', $q) . $jsonp_after;
 
 ?>
