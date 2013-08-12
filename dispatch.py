2cf89744cd5aa9667dfa3f0f657e7b155d22d5a0
Marian Steinbach <marian@sendung.de>
2013-08-12 21:46:35 +0200

    Verzicht auf flup

--- dispatch.py
+++ dispatch.py
@@ -1,18 +1,13 @@
-#!/var/www/openorgdata.sendung.de/venv/bin/python
-
 # encoding: utf-8
 
-from flup.server.fcgi import WSGIServer
 from flask import Flask
 
 app = Flask(__name__)
 app.debug = True
 
-print "default"
 
 @app.route('/')
 def home():
-    print "home"
     return 'Hallo Home!'
 
 
@@ -22,5 +17,4 @@ def hello_world(foo):
     return 'Hallo %s' % foo
 
 if __name__ == '__main__':
-    WSGIServer(app, bindAddress='/tmp/openorgdata-fcgidsock').run()
-    #WSGIServer(app).run()
+    app.run()
