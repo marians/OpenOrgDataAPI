25725e6fc8668f99232159491829a9d5a911d14c
Marian Steinbach <marian@sendung.de>
2013-08-12 22:14:57 +0200

    URL Params parsen, pyes eingefügt

--- dispatch.py
+++ dispatch.py
@@ -1,9 +1,14 @@
 # encoding: utf-8
 
 from flask import Flask
+from flask import request
+import pyes
+import json
 
 app = Flask(__name__)
 app.debug = True
+
+esconn = pyes.ES('127.0.0.1:9200')
 
 
 @app.route('/')
@@ -11,10 +16,12 @@ def home():
     return 'Hallo Home!'
 
 
-@app.route('/<foo>')
-def hello_world(foo):
+@app.route('/api/')
+def hello_world():
     print "hello_world"
-    return 'Hallo %s' % foo
+    q = request.args.get('q', '')
+    return 'Hallo %s' % q
+
 
 if __name__ == '__main__':
     app.run()
